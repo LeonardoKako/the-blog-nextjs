71af59375225ff4662f3998bb19c78ff2f76e244
LeonardoKako <fleithkako@gmail.com>
2025-05-26 11:06:23 -0300

    errorPage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,15 @@
-import clsx from "clsx";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export default function NotFoundPage() {
   return (
     <>
-      <title>Página não encontrada</title>
-      <div
-        className={clsx(
-          "min-h-[320px]",
-          "bg-slate-900",
-          "text-slate-100",
-          "mb-16",
-          "p-8",
-          "rounded-xl",
-          "flex",
-          "items-center",
-          "justify-center"
-        )}
-      >
-        <div>
-          <h1 className="text-7xl/normal mb-8 font-extrabold text-center">
-            404
-          </h1>
+      <ErrorMessage
+        content={
           <p>Erro 404 - A página que você está tentando acessar não existe</p>
-        </div>
-      </div>
+        }
+        contentTitle="404"
+        pageTitle="Página não encontrada"
+      />
     </>
   );
 }
