9654535748f470943d82b8d1db0f1bd9244e4ec0
LeonardoKako <fleithkako@gmail.com>
2025-06-06 11:24:23 -0300

    admin new post

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -6,22 +6,83 @@ import { InputText } from "@/components/InputText";
 import { MarkdownEditor } from "@/components/MarkdownEditor";
 import { useState } from "react";
 import { ImageUploader } from "../ImageUploader";
+import { PublicPost } from "@/dto/post/dto";
 
-export function ManagePostForm() {
-  const [contentValue, setContentValue] = useState("Este é o exemplo");
+type ManagePostFormProps = {
+  publicPost?: PublicPost;
+};
+
+export function ManagePostForm({ publicPost }: ManagePostFormProps) {
+  const [contentValue, setContentValue] = useState(publicPost?.content || "");
 
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
-        <InputText placeholder="Digite seu nome" labelText="Nome" />
-        <InputCheckbox labelText="Receba?" />
-        <ImageUploader />
+        <InputText
+          labelText="ID"
+          name="id"
+          placeholder="ID gerado automaticamente"
+          type="text"
+          readOnly
+          defaultValue={publicPost?.id || ""}
+        />
+
+        <InputText
+          labelText="Slug"
+          name="slug"
+          placeholder="Slug gerado automaticamente"
+          type="text"
+          readOnly
+          defaultValue={publicPost?.slug || ""}
+        />
+
+        <InputText
+          labelText="Titulo"
+          name="title"
+          placeholder="Digite o título"
+          type="text"
+          defaultValue={publicPost?.title || ""}
+        />
+
+        <InputText
+          labelText="Autor"
+          name="author"
+          placeholder="Digite o nome do autor"
+          type="text"
+          defaultValue={publicPost?.author || ""}
+        />
+
+        <InputText
+          labelText="Excerto"
+          name="excerpt"
+          placeholder="Digite o resumo"
+          type="text"
+          defaultValue={publicPost?.excerpt || ""}
+        />
+
         <MarkdownEditor
           labelText="Conteúdo"
-          disabled={false}
-          textAreaName="content"
           value={contentValue}
+          textAreaName="content"
           setValue={setContentValue}
+          disabled={false}
+        />
+
+        <ImageUploader />
+
+        <InputText
+          labelText="URL da image de capa"
+          name="coverImageUrl"
+          placeholder="Digite a url da imagem"
+          type="text"
+          defaultValue={publicPost?.coverImageUrl || ""}
+        />
+
+        <InputCheckbox
+          labelText="Publicar?"
+          name="published"
+          type="checkbox"
+          defaultChecked={publicPost?.published || false}
         />
       </div>
       <div className="mt-4">
