2ce92a4261f3a9a3ce309f59bdcf3201e1141ab3
LeonardoKako <fleithkako@gmail.com>
2025-06-06 10:18:18 -0300

    showing image

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -4,12 +4,13 @@ import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
 import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef, useTransition } from "react";
+import { useRef, useState, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [isUploading, startTransition] = useTransition();
+  const [imgUrl, setImgUrl] = useState("");
 
   function handleChooseFile() {
     if (!fileInputRef.current) return;
@@ -18,15 +19,22 @@ export function ImageUploader() {
 
   function handleChange() {
     toast.dismiss();
-    if (!fileInputRef.current) return;
+    if (!fileInputRef.current) {
+      setImgUrl("");
+      return;
+    }
     const fileInput = fileInputRef.current;
     const file = fileInput?.files?.[0];
 
-    if (!file) return;
+    if (!file) {
+      setImgUrl("");
+      return;
+    }
 
     if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
       toast.error(`Image muito grande. Máx:${IMAGE_UPLOAD_MAX_SIZE / 1024}KB`);
       fileInput.value = "";
+      setImgUrl("");
       return;
     }
 
@@ -39,10 +47,12 @@ export function ImageUploader() {
       if (result.error) {
         toast.error(result.error);
         fileInput.value = "";
+        setImgUrl("");
         return;
       }
 
-      toast.success(result.url);
+      setImgUrl(result.url);
+      toast.success("Imagem enviada");
     });
 
     fileInput.value = "";
@@ -50,10 +60,24 @@ export function ImageUploader() {
 
   return (
     <div className="flex flex-col gap-2 py-4">
-      <Button type="button" className="self-start" onClick={handleChooseFile}>
+      <Button
+        type="button"
+        className="self-start"
+        onClick={handleChooseFile}
+        disabled={isUploading}
+      >
         <ImageUpIcon />
         Enviar imagem
       </Button>
+      {!!imgUrl && (
+        <div className="flex flex-col gap-4">
+          <p>
+            <b>URL:</b> {imgUrl}
+          </p>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img className="rounded-lg max-w-[200px]" src={imgUrl} alt="Image" />
+        </div>
+      )}
       <input
         onChange={handleChange}
         ref={fileInputRef}
@@ -61,6 +85,7 @@ export function ImageUploader() {
         name="file"
         type="file"
         accept="image/*"
+        disabled={isUploading}
       />
     </div>
   );
