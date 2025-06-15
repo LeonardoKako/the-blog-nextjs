66f91d668defd120c922c420aa6d11599a28a23e
LeonardoKako <fleithkako@gmail.com>
2025-06-15 10:28:14 -0300

    create delete update in post-repository

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,15 +1,10 @@
 "use server";
 
-import { drizzleDb } from "@/db/drizzle";
-import { postsTable } from "@/db/drizzle/schemas";
 import { postRepository } from "@/repositories/post";
-import { asyncDelay } from "@/utils/async-delay";
 import { logColor } from "@/utils/log-color";
-import { eq } from "drizzle-orm";
 import { revalidateTag } from "next/cache";
 
 export async function deletePostAction(id: string) {
-  await asyncDelay(2000);
   logColor(id);
 
   if (!id || typeof id !== "string") {
@@ -18,15 +13,22 @@ export async function deletePostAction(id: string) {
     };
   }
 
-  const post = await postRepository.findById(id).catch(() => undefined);
+  let post;
 
-  if (!post) {
+  try {
+    post = await postRepository.delete(id);
+    await postRepository.delete(id);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        error: e.message,
+      };
+    }
+
     return {
-      error: "Esse post não existe",
+      error: "Erro desconhecido",
     };
   }
-
-  await drizzleDb.delete(postsTable).where(eq(postsTable.id, id));
 
   revalidateTag("posts");
   revalidateTag(`post-${post.slug}`);
