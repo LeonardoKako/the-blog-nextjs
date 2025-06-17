1c5d2b33456e1b60cc7a35166d4bd12f60c353b2
LeonardoKako <fleithkako@gmail.com>
2025-06-17 10:50:23 -0300

    criando hashed senha

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -10,6 +10,21 @@ type loginActionState = {
 export async function loginAction(state: loginActionState, formData: FormData) {
   await asyncDelay(3000);
 
+  if (!(formData instanceof FormData)) {
+    return {
+      username: "",
+      error: "",
+    };
+  }
+
+  // Dados vindo do form
+  const username = formData.get("username")?.toString() || "";
+  const password = formData.get("password")?.toString() || "";
+
+  // Aqui eu checaria se o usuário existe na base de dados
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = "";
+
   return {
     username: "",
     error: "",
